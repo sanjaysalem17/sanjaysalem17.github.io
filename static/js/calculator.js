f1758d08c9f470908b6a97645cb73f714c110802
Sanjay Salem <sanjaysalem17@gmail.com>
2020-09-26 19:44:57 -0500

    adding calculator

---
+++ static/js/calculator.js
@@ -0,0 +1,83 @@
+var prev = 0
+var curr = 0
+var prevop = "plus"
+var prevbutton = false
+var prevequals = false
+
+function singOp(op) {
+    if (prevequals) {
+        if (op == 'negate') prev *= -1
+        else if (op == 'square') prev *= prev
+        else if (op == 'sqrt') prev = Math.sqrt(prev)
+        document.getElementById('calc-display').value = prev
+    } else {
+        if (op == 'negate') curr *= -1
+        else if (op == 'square') curr *= curr
+        else if (op == 'sqrt') curr = Math.sqrt(curr)
+        document.getElementById('calc-display').value = curr
+    }
+    prevequals = false
+    console.log("neg")
+}
+
+function operate() {
+    switch (prevop) {
+        case 'equals':
+            break;
+        case 'plus':
+            prev += curr
+            break
+        case 'minus':
+            prev -= curr
+            break
+        case 'times':
+            prev *= curr
+            break
+        case 'mod':
+            prev %= curr
+            break
+        case 'divide':
+            if (curr == 0) {
+                prev = 0
+                curr = 0
+                alert("Arithmetic Error: Divide by Zero")
+            }
+            else {prev /= curr}
+            break
+        default:
+            console.log('')
+    }
+}
+
+function doMath(num, op, type) {
+    if (type){
+        if(prevequals) prev = 0
+        curr *= 10
+        curr += num
+        prevbutton = false
+        document.getElementById('calc-display').value = curr
+    } else if (op == 'clear') {
+        prevop = "plus"
+        prev = 0
+        curr = 0
+        prevbutton = false
+        prevequals = false
+        document.getElementById('calc-display').value = prev
+    }
+    else {
+        if(!prevbutton) operate()
+        if (op == 'equals') {
+            prevop = 'plus'
+            curr = 0
+            prevequals = true
+        } else {
+            if(!prevbutton) curr = 0
+            prevop = op
+            prevequals = false   
+        }
+        document.getElementById('calc-display').value = prev
+        prevbutton = true
+        console.log(op)
+        
+    }
+}
